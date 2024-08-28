1e2368b2421599b726f3db03c6ae370214e4ae5e
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-08-28 13:45:00 +0200

    add wrapper around both examples

---
+++ code/analyses.R
@@ -0,0 +1,6 @@
+cat("Running analyses for site 690 \n")
+source("code/analysis_site690.R")
+cat("Running analysis for Steinbruch Schmidt\n")
+source("code/analysis_steinbruch_schmidt.R")
+
+Cat("done! Outputs are in the folder figs/")
